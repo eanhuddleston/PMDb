f16d1c82740c9b5982ceb76ab708aac918c9d1a7
Ean Huddleston <ean.huddleston@gmail.com>
2013-05-08 10:13:30 -0700

    Make RottenT's api call work

--- app/assets/javascripts/views/addMovieView.js
+++ app/assets/javascripts/views/addMovieView.js
@@ -19,28 +19,29 @@ PM.Views.AddMovieView = Backbone.View.extend ({
     console.log("hello!");
 
     // var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=oblivion&apikey=47g5x5cc7h5mf694cygt3jgd";
-    // $.get(url, function(response) {
+    // $.getJSON(url, function(response) {
     //   console.log(response);
     // });
     
+    var movie_title = "Oblivion";
+
     $.ajax({
       type: "GET",
-      url: "http://api.rottentomatoes.com/api/public/v1.0/movies.json",
-      contentType: "application/json",
-      data: JSON.stringify({
-          q: "Oblivion",
+      url: "http://api.rottentomatoes.com/api/public/v1.0/movies",
+      data: {
+          q: movie_title,
           apikey: "47g5x5cc7h5mf694cygt3jgd"
-      }),
-      dataType: "json",
+      },
+      dataType: "jsonp",
       success: function(response) {
-        // Put the plain text in the PRE tag.
         console.log(response);
+        
       }
     });
 
   }, 
 
   process: function (data) {
-
+    console.log("made it");
   }
 });
